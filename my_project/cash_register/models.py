c4d91d8e007abff1f3a8f516cd846227b6dc3312
Беликов Владимир <belikrastr@yandex.ru>
2023-12-07 02:49:00 +0300

    Добавление модели

--- my_project/cash_register/models.py
+++ my_project/cash_register/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Наименование')
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name='Стоимость'
+        )
+
+    def __str__(self):
+        return self.title
--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cash_register.apps.CashRegisterConfig',
 ]
 
 MIDDLEWARE = [
